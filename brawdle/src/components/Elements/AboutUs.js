2198eb26160626e87618379f542f29495c4493f9
DajWaj <dajwaj.glowny@gmail.com>
2024-05-31 01:09:11 +0200

    Brawldle V0.6.4
    Linki do githubów

--- brawdle/src/components/Elements/AboutUs.js
+++ brawdle/src/components/Elements/AboutUs.js
@@ -12,7 +12,7 @@ const AboutUs = () => {
                         <h5>Front-end Developer & <span>Designer</span></h5>
                         <p>Hey there! I'm passionate about crafting visually stunning and interactive user interfaces that bring websites to life. With a keen eye for design and a knack for problem-solving, I specialize in turning ideas into captivating web experiences. From pixel-perfect layouts to smooth animations, I thrive on the creative challenges of front-end development. My Front-end is really Fronty.</p>
                         <div className="data">
-                            <a href="#" className="hire">Hire Me</a>
+                            <a href="https://github.com/xiega" className="hire">Hire Me</a>
                         </div>
                     </div>
                 </div>
@@ -24,7 +24,7 @@ const AboutUs = () => {
                         <h5>Back-end Developer & <span>Silly goose</span></h5>
                         <p>Hi, I'm your go-to person for all things server-side magic! Behind the scenes, I architect robust and scalable systems that power web applications with speed and efficiency. Proficient in languages like Python and Node.js, I love diving into databases, APIs, and server logic to build rock-solid back-end solutions. Does silly stuff sometimes. Knows he can do hard things, just doesn't want to. Won't elaborate.</p>
                         <div className="data">
-                            <a href="#" className="hire">Hire Me</a>
+                            <a href="https://github.com/DajWaj" className="hire">Hire Me</a>
                         </div>
                     </div>
                     <img src={`${process.env.PUBLIC_URL}/media/goose.png`} alt="Goose" className={'pic'}/>
@@ -38,7 +38,7 @@ const AboutUs = () => {
                         <h5>Tester Master & <span>Documenter</span></h5>
                         <p>Greetings! I'm on a mission to ensure the quality and integrity of every line of code and every feature of our applications. As a meticulous tester and documentarian, I meticulously analyze, test, and document software to ensure it meets the highest standards of quality and reliability. I'm committed to uncovering bugs, glitches, and inconsistencies to ensure a seamless user experience.</p>
                         <div className="data">
-                            <a href="#" className="hire">Hire Me</a>
+                            <a href="https://github.com/Dzilne" className="hire">Hire Me</a>
                         </div>
                     </div>
                 </div>
