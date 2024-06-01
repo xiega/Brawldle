b01c930f6577f3c6dfd58aaf1fe5ba1b4899310f
Dzilne <90028788+Dzilne@users.noreply.github.com>
2024-06-01 16:33:45 +0200

    Brawdle V0.7.9
    Edit BrawlerList.test.js

--- brawdle/src/components/Elements/BrawlerList.test.js
+++ brawdle/src/components/Elements/BrawlerList.test.js
@@ -7,10 +7,8 @@ import axios from 'axios';
 import AxiosMockAdapter from 'axios-mock-adapter';
 import BrawlerList from './BrawlerList';
 
-// Create a mock Axios instance
 const mock = new AxiosMockAdapter(axios);
 
-// Sample data to be returned by the mock API
 const brawlers = [
   {
     name: 'Shelly',
@@ -34,20 +32,16 @@ describe('BrawlerList Component', () => {
   });
 
   it('should render the list of brawlers from API', async () => {
-    // Mock the GET request to return the sample data
     mock.onGet('http://localhost:4000/api/brawlers-list').reply(200, brawlers);
 
     render(<BrawlerList />);
 
-    // Wait for the elements to be rendered
     await waitFor(() => expect(screen.getByText('Shelly')).toBeInTheDocument());
     await waitFor(() => expect(screen.getByText('Colt')).toBeInTheDocument());
 
-    // Check if the brawler names are rendered
     expect(screen.getByText('Shelly')).toBeInTheDocument();
     expect(screen.getByText('Colt')).toBeInTheDocument();
 
-    // Check if the brawler images are rendered
     expect(screen.getByAltText('Shelly')).toBeInTheDocument();
     expect(screen.getByAltText('Colt')).toBeInTheDocument();
   });
@@ -55,12 +49,10 @@ describe('BrawlerList Component', () => {
   it('should display an error message if the API call fails', async () => {
     console.error = jest.fn(); // Mock console.error to avoid cluttering the test output
 
-    // Mock the GET request to return an error
     mock.onGet('http://localhost:4000/api/brawlers-list').reply(500);
 
     render(<BrawlerList />);
 
-    // Wait for the console error to be called
     await waitFor(() => expect(console.error).toHaveBeenCalledWith('Błąd podczas pobierania Brawlerów:', expect.any(Error)));
   });
 });
